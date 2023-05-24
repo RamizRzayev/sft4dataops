5d42d992d776fac0aaeaad92955a1e1511790b5e
RamizRzayev <ramrzayev@gmail.com>
2023-05-24 10:03:45 +0400

    'fake'

---
+++ test/integration/test_data_pipeline_emp.py
@@ -0,0 +1,25 @@
+#import psycopg2.extras as p
+from data_test_ci.data_pipeline import run
+from data_test_ci.utils.db import WarehouseConnection
+from data_test_ci.utils.sde_config import get_warehouse_creds
+
+class TestDataPipeline_emp:
+
+    def setup_method(self, test_data_pipeline):
+        None
+        
+    def teardown_method(self, test_data_pipeline):
+        None
+
+
+    def test_data_pipeline(self):
+        run()
+        names_expected=['id','deptid','name','email']
+        with WarehouseConnection(
+                get_warehouse_creds()
+        ).managed_cursor() as curr:
+
+            curr.execute("Select * from emp")
+            column_names = list(map(lambda x: x[0], curr.description))
+
+        assert column_names == names_expected
