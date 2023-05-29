5552129300e0036bbac96108fb3242c3572389a6
RamizRzayev <ramrzayev@gmail.com>
2023-05-29 17:27:59 +0400

    fake comment

--- test/integration/test_data_pipeline_info_v.py
+++ test/integration/test_data_pipeline_info_v.py
@@ -5,11 +5,13 @@ from data_test_ci.utils.sde_config import get_warehouse_creds
 
 class TestDataPipeline_info_v:
 
+
     def setup_method(self, test_data_pipeline):
         None
-        
+        #comment         
     def teardown_method(self, test_data_pipeline):
         None
+
 
 
     def test_data_pipeline(self):
